e5474598fe8571fc81d9a5f29617533942208808
Andre A. Marin <andre@alexamarin.com>
2020-07-19 19:41:02 -0500

    Add finished work for reversible_function

--- reversible_function/src/reversible_function.cpp
+++ reversible_function/src/reversible_function.cpp
@@ -7,7 +7,7 @@
 //============================================================================
 
 #include <iostream>
-#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -21,6 +21,57 @@ struct Params {
   double a;
   double b;
   double c;
+};
+
+class FunctionPart {
+public:
+	FunctionPart() = delete;
+	FunctionPart(char new_operation, double new_value):
+		operation(new_operation),value(new_value) {}
+
+	double Apply( double source_value) const {
+		if ( operation == '+' ) {
+			return source_value + value;
+		}
+		else {
+			return source_value - value; }
+	}
+
+	void Invert() {
+		operation = ( operation == '+' ) ? '-' : '+';
+	}
+
+private:
+	char operation;
+	double value;
+};
+
+class Function {
+public :
+	Function() = default;
+
+	void AddPart(char operation, double value) {
+		parts.push_back ({operation, value});
+	}
+
+	double Apply ( double value) const {
+		for (const FunctionPart & part : parts) {
+			value = part.Apply (value);
+		}
+
+		return value;
+	}
+
+	void Invert () {
+		for (FunctionPart & part : parts) {
+			part.Invert ();
+		}
+
+		reverse (begin(parts), end(parts));
+	}
+
+private :
+	vector <FunctionPart> parts;
 };
 
 Function MakeWeightFunction(const Params& params,
