5b8e7f3f5dbd5db9309017bdf09395257191877a
Andre A. Marin <andre@alexamarin.com>
2020-07-31 13:42:42 -0500

    Add rational_interface part 4

--- rational_class/rational_interface.cpp
+++ rational_class/rational_interface.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
 #include <numeric>
-
-using namespace std;
+#include <string>
 
 class Rational {
 public:
@@ -11,6 +13,7 @@ public:
 
     Rational (int numerator, int denominator):
         _numerator(numerator), _denominator(denominator) {
+        if(_denominator == 0) _numerator = 1;
         ctor_init();  
     }
 
@@ -43,11 +46,11 @@ public:
         const auto rhs_new_num = rhs.Numerator() * rhs_new_num_mult;
         const auto new_numerator = new_num - rhs_new_num;
 
-        // cout << "new_num_mult: " << new_num_mult << endl; 
-        // cout << "rhs_new_num_mult: " << rhs_new_num_mult << endl;
-        // cout << "new_num: " << new_num << endl;
-        // cout << "rhs_new_num: " << rhs_new_num << endl;
-        // cout << "operater-: numerator: " << new_numerator << ", denominator: " << lcm_val << endl;
+        // std::cout << "new_num_mult: " << new_num_mult << std::endl; 
+        // std::cout << "rhs_new_num_mult: " << rhs_new_num_mult << std::endl;
+        // std::cout << "new_num: " << new_num << std::endl;
+        // std::cout << "rhs_new_num: " << rhs_new_num << std::endl;
+        // std::cout << "operater-: numerator: " << new_numerator << ", denominator: " << lcm_val << std::endl;
 
         return Rational(new_numerator, lcm_val);    
     }
@@ -82,22 +85,45 @@ private:
     void ctor_init() {
         _multiplier = multiplier(); 
         _gcd_val = std::gcd(_numerator, _denominator);
-        // cout << "Inital: numerator: " << _numerator << ", denominator: " << _denominator 
-        // << ", multiplier: " << _multiplier << ", gcd: " << _gcd_val << endl;
+        // std::cout << "Inital: numerator: " << _numerator << ", denominator: " << _denominator 
+        // << ", multiplier: " << _multiplier << ", gcd: " << _gcd_val << std::endl;
         _denominator = _denominator < 0 ? -1 * _denominator : _denominator;
         _numerator = _numerator < 0 ? -1 * _numerator : _numerator;
         _denominator = _denominator / _gcd_val;
         _numerator = (_numerator * _multiplier)/_gcd_val;
-        // cout << "After: numerator: " << _numerator << ", denominator: " << _denominator 
-        // << ", multiplier: " << _multiplier << ", gcd: " << _gcd_val << endl;
+        // std::cout << "After: numerator: " << _numerator << ", denominator: " << _denominator 
+        // << ", multiplier: " << _multiplier << ", gcd: " << _gcd_val << std::endl;
     }
 };
+
+std::istream& operator>> (std::istream& stream, Rational& rational) {
+    if(stream.peek() == std::char_traits<char>::eof()) {
+        return stream;
+    }
+
+    int numerator = 0;
+    stream >> numerator;
+
+    stream.ignore(1); 
+
+    int denominator = 0;
+    stream >> denominator;
+
+    rational = Rational(numerator, denominator);
+    return stream; 
+}
+
+std::ostream& operator<< (std::ostream& stream, const Rational& rational) {
+   // std::cout << rational.Numerator() << "/" << rational.Denominator() << std::endl;
+    stream << rational.Numerator() << "/" << rational.Denominator();
+    return stream;
+}
 
 int main() {
     {
         const Rational r(3, 10);
         if (r.Numerator() != 3 || r.Denominator() != 10) {
-            cout << "Rational(3, 10) != 3/10" << endl;
+            std::cout << "Rational(3, 10) != 3/10" << std::endl;
             return 1;
         }
     }
@@ -105,7 +131,7 @@ int main() {
     {
         const Rational r(8, 12);
         if (r.Numerator() != 2 || r.Denominator() != 3) {
-            cout << "Rational(8, 12) != 2/3" << endl;
+            std::cout << "Rational(8, 12) != 2/3" << std::endl;
             return 2;
         }
     }
@@ -113,7 +139,7 @@ int main() {
     {
         const Rational r(-4, 6);
         if (r.Numerator() != -2 || r.Denominator() != 3) {
-            cout << "Rational(-4, 6) != -2/3" << endl;
+            std::cout << "Rational(-4, 6) != -2/3" << std::endl;
             return 3;
         }
     }
@@ -121,7 +147,7 @@ int main() {
     {
         const Rational r(4, -6);
         if (r.Numerator() != -2 || r.Denominator() != 3) {
-            cout << "Rational(4, -6) != -2/3" << endl;
+            std::cout << "Rational(4, -6) != -2/3" << std::endl;
             return 3;
         }
     }
@@ -129,7 +155,7 @@ int main() {
     {
         const Rational r(0, 15);
         if (r.Numerator() != 0 || r.Denominator() != 1) {
-            cout << "Rational(0, 15) != 0/1" << endl;
+            std::cout << "Rational(0, 15) != 0/1" << std::endl;
             return 4;
         }
     }
@@ -137,7 +163,7 @@ int main() {
     {
         const Rational defaultConstructed;
         if (defaultConstructed.Numerator() != 0 || defaultConstructed.Denominator() != 1) {
-            cout << "Rational() != 0/1" << endl;
+            std::cout << "Rational() != 0/1" << std::endl;
             return 5;
         }
     }
@@ -147,7 +173,7 @@ int main() {
         Rational r2(2, 3);
         bool equal = r1 == r2;
         if (!equal) {
-            cout << "4/6 != 2/3" << endl;
+            std::cout << "4/6 != 2/3" << std::endl;
             return 1;
         }
     }
@@ -158,7 +184,7 @@ int main() {
         Rational c = a + b;
         bool equal = c == Rational(2, 1);
         if (!equal) {
-            cout << "2/3 + 4/3 != 2" << endl;
+            std::cout << "2/3 + 4/3 != 2" << std::endl;
             return 2;
         }
     }
@@ -169,7 +195,7 @@ int main() {
         Rational c = a - b;
         bool equal = c == Rational(31, 63);
         if (!equal) {
-            cout << "5/7 - 2/9 != 31/63" << endl;
+            std::cout << "5/7 - 2/9 != 31/63" << std::endl;
             return 3;
         }
     }
@@ -180,7 +206,7 @@ int main() {
         Rational c = a * b;
         bool equal = c == Rational(8, 9);
         if (!equal) {
-            cout << "2/3 * 4/3 != 8/9" << endl;
+            std::cout << "2/3 * 4/3 != 8/9" << std::endl;
             return 1;
         }
     }
@@ -191,11 +217,50 @@ int main() {
         Rational c = a / b;
         bool equal = c == Rational(2, 3);
         if (!equal) {
-            cout << "5/4 / 15/8 != 2/3" << endl;
+            std::cout << "5/4 / 15/8 != 2/3" << std::endl;
             return 2;
         }
     }
 
-    cout << "OK" << endl;
+    {
+        std::ostringstream output;
+        output << Rational(-6, 8);
+        if (output.str() != "-3/4") {
+            std::cout << "Rational(-6, 8) should be written as \"-3/4\"" << std::endl;
+            return 1;
+        }
+    }
+
+    {
+        std::istringstream input("5/7");
+        Rational r;
+        input >> r;
+        bool equal = r == Rational(5, 7);
+        if (!equal) {
+            std::cout << "5/7 is incorrectly read as " << r << std::endl;
+            return 2;
+        }
+    }
+
+    {
+        std::istringstream input("5/7 10/8");
+        Rational r1, r2;
+        input >> r1 >> r2;
+        bool correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
+        if (!correct) {
+            std::cout << "Multiple values are read incorrectly: " << r1 << " " << r2 << std::endl;
+            return 3;
+        }
+
+        input >> r1;
+        input >> r2;
+        correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
+        if (!correct) {
+            std::cout << "Read from empty stream shouldn't change arguments: " << r1 << " " << r2 << std::endl;
+            return 4;
+        }
+    }
+
+    std::cout << "OK" << std::endl;
     return 0;
 }
